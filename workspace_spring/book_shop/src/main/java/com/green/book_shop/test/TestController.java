692b90cb7f73f960e43936562d8f3beb4e5a9463
Hobbyer <uskimeunyeob@gmail.com>
2025-03-19 16:47:42 +0900

    리액트에서 보내는 업로드 파일을 받고, 받은 파일을 내 컴퓨터에 저장하는 로직 작성

--- workspace_spring/book_shop/src/main/java/com/green/book_shop/test/TestController.java
+++ workspace_spring/book_shop/src/main/java/com/green/book_shop/test/TestController.java
@@ -3,6 +3,9 @@ package com.green.book_shop.test;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -13,9 +16,17 @@ public class TestController {
 
   // 첨부파일 연습1
   @PostMapping("/upload1")
-  public String upload1(@RequestParam("firstFile")MultipartFile file){
+  // @RequestParam: 요청 파라미터를 전달받을 수 있는 어노테이션
+  // MultipartFile: 첨부파일을 전달받을 수 있는 객체
+  public String upload1(@RequestParam("firstFile") MultipartFile file) throws IOException {
     // 파일 처리 로직
     System.out.println(file.getOriginalFilename());
+
+    // 내 컴퓨터에 저장
+    String uploadPath = "E:\\Study\\Green_Academy\\WorkSpace\\workspace_spring\\book_shop\\src\\main\\resources\\upload\\"; // 파일 저장 경로
+    String fileName = file.getOriginalFilename(); // 파일 이름
+    file.transferTo(new File(uploadPath +fileName)); // 파일 저장
+
     return file.getOriginalFilename();
   }
 }
