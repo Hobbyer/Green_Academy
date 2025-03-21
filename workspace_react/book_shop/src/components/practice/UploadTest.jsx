b77a41195589bdeb5e2c789a8a6f63fbc6f4da6e
Hobbyer <uskimeunyeob@gmail.com>
2025-03-21 11:52:28 +0900

    단일 파일 업로드, 다중 파일 업로드 구현

--- workspace_react/book_shop/src/components/practice/UploadTest.jsx
+++ workspace_react/book_shop/src/components/practice/UploadTest.jsx
@@ -5,17 +5,40 @@ const UploadTest = () => {
   // 첨부파일 input 태그에서 선택한 파일을 저장할 변수
   const [firstFile, setFirstFile] = useState(null);
 
-  const fileConfig = {headers : {'Content-Type' : 'multipart/form-data'}}; // 파일 전송 시 필수
+  // 첨부파일 다중파일을 저장할 변수
+  const [secondFile, setSecondFile] = useState(null);
+
+
+  // 파일 전송 시 필요한 설정
+  const fileConfig = {headers : {'Content-Type' : 'multipart/form-data'}};
 
   const sendFile = () => {
     // FormData 객체 생성
     const formData = new FormData();
     // append 메소드로 key-value 형태로 데이터를 추가한다.
-    formData.append('firstFile', firstFile);
+    formData.append('secondFiles', secondFile);
 
-    // axios.post('url', data, config)
-    // config : { headers : { 'Content-Type' : 'multipart/form-data' }}
     axios.post('/api/test/upload1', formData, fileConfig)
+    .then(res=>{
+      console.log(res.data);
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  // 다중 파일 전송
+  const sendFiles = () => {
+    const formData = new FormData();
+
+    if (secondFile !== null) {
+
+    for(let i=0; i<secondFile.length; i++){
+      formData.append('secondFiles', secondFile[i]);
+    }
+
+  }
+
+    axios.post('/api/test/upload2', formData, fileConfig)
     .then(res=>{
       console.log(res.data);
     }).catch(error=>{
@@ -25,10 +48,17 @@ const UploadTest = () => {
 
   return (
     <div>
+      {/* 단일파일 업로드 */}
       <input type="file" onChange={(e)=>{ 
         setFirstFile(e.target.files[0]) // 선택한 파일을 변수에 저장
-      }} multiple />
+      }} multiple /> {/* multiple 속성을 추가하면 여러 파일을 선택할 수 있다.  */}
       <button type='button' onClick={sendFile} >파일 전송1</button>
+
+      {/* 다중파일 업로드 */}
+      <input type="file" onChange={(e)=>{
+        setSecondFile(e.target.files) // 선택한 파일들을 변수에 저장
+      }} multiple />
+      <button type='button' onClick={sendFiles} >다중 파일 전송</button>
     </div>
   )
 }
